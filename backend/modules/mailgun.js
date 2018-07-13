355886b2c1e3a50842b43fcebdc0aa69e1bce63d
dlewita-pro <d.lewita@webankieta.pl>
2018-07-13 14:46:32 +0200

    small fix in backend

--- backend/modules/mailgun.js
+++ backend/modules/mailgun.js
@@ -2,7 +2,7 @@ const config = require('./config');
 
 module.exports = (data) => {
     const api_key = config.mailgun;
-    const DOMAIN = 'mailgun.dami95.pl';
+    const DOMAIN = 'mailgun.dami95.pl'; //@TODO it should be also in config file
     const mailgun = require('mailgun-js')({apiKey: api_key, domain: DOMAIN});
 
     return new Promise((resolve, reject) => {
